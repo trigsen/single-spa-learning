9f483b15da4bbbc2489a7221385590af72a064b6
Roman Triputko <romantriputko@gmail.com>
2022-11-03 23:36:02 +0100

    adds layout engine

--- packages/app-root/src/trgsn-root-config.ts
+++ packages/app-root/src/trgsn-root-config.ts
@@ -1,22 +1,41 @@
 import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from 'single-spa-layout';
 
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    )as any,
-  activeWhen: (location) => location.pathname === '/',
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     )as any,
+//   activeWhen: (location) => location.pathname === '/',
+// });
+//
+// registerApplication({
+//   name: "@trgsn/todo",
+//   app: () => System.import("@trgsn/todo") as any,
+//   activeWhen: ["/todo"]
+// });
+
+
+const routes = constructRoutes(document.querySelector('#single-spa-layout'));
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    if (name === '@single-spa/welcome') {
+      return System.import("https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js")
+    }
+
+    return System.import(name);
+  },
 });
+const layoutEngine = constructLayoutEngine({ routes, applications });
 
-registerApplication({
-  name: "@trgsn/todo",
-  app: () => System.import("@trgsn/todo") as any,
-  activeWhen: ["/todo"]
-});
+layoutEngine.activate()
+applications.forEach(registerApplication);
 
-
-
-start({
-  urlRerouteOnly: true,
-});
+layoutEngine.activate()
+start();
